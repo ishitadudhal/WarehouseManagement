a8e447714718580e15f18bcbcc681e780796bc9f
Ishita Dudhal <ishitadudhal@gmail.com>
2026-09-08 12:26:10 +0200

    changes with structure

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Import all models here so Flask-SQLAlchemy can find them when creating tables
-from app.models.shelves import Shelf
-from app.models.items   import Item
-from app.models.orders  import Order
-from app.models.events  import Event
--- run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-import app.models  # make sure all models are imported before db.create_all()
-
-flask_app = create_app()
-
-if __name__ == '__main__':
-    flask_app.run(debug=True, port=5000)
